269239dd0ffe6b0a478abd6592f717891ec0bff9
Administrator <345772760@qq.com>
2021-07-01 14:00:24 +0800

    安装完bootstrap

--- src/main/java/com/example/demo1/controller/HelloController.java
+++ src/main/java/com/example/demo1/controller/HelloController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
     }
 }
